1416ca0e44b79c3391c10b6c6a564624c318cb1c
Max Rogério <max10rogerio@hotmail.com>
2020-06-29 17:17:03 -0300

    fix: form keys in hook

--- src/hooks/useApplyProps/useApplyProps.ts
+++ src/hooks/useApplyProps/useApplyProps.ts
@@ -5,14 +5,17 @@ export interface IUseApplyProps {
   errors: Record<string, any>;
 }
 
-export type ApplyProps = <T = any>(id: string) => TextFieldProps | T;
+export type ApplyProps<K> = (id: keyof K) => TextFieldProps & { [key: string]: any };
 
-export const useApplyProps = ({ register, errors }: IUseApplyProps): ApplyProps => {
-  return (id: string) => ({
-    id,
-    name: id,
-    inputRef: register,
-    error: !!errors[id]?.message,
-    helperText: errors[id]?.message,
-  });
+export const useApplyProps = <K = any>({ register, errors }: IUseApplyProps): ApplyProps<K> => {
+  return (key: keyof K) => {
+    const id = String(key);
+    return {
+      id: id,
+      name: id,
+      inputRef: register,
+      error: !!errors[id]?.message,
+      helperText: errors[id]?.message,
+    };
+  };
 };
