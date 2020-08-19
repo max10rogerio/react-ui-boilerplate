b1ac801571a55d74a8ae62b345dc39e4c39e3cd4
Marcio Cesar Corrêa <marciocesarcorrea@gmail.com>
2020-08-19 13:55:05 -0300

    feat: auto theme

--- src/components/Theme/Theme.tsx
+++ src/components/Theme/Theme.tsx
@@ -6,8 +6,10 @@ import { useAppSelector } from "store";
 import { darkTheme, defaultTheme } from "assets/themes";
 
 export const Theme: FC = ({ children }) => {
+  const darkOS = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const isDarkTheme = useAppSelector((state) => state.layout.isDarkTheme);
-  const theme = isDarkTheme ? darkTheme : defaultTheme;
+  const theme =
+    isDarkTheme === undefined ? (darkOS ? darkTheme : defaultTheme) : isDarkTheme ? darkTheme : defaultTheme;
 
   return (
     <ThemeProvider theme={theme}>
